3f882a1d6e585dd8b286dc6c9248d11899cd444a
Deborah <deborah.bpaz@gmail.com>
2025-04-02 15:46:57 -0300

    teste

---
+++ src/main/java/projeto/dio/projeto/java/ProjetoJavaApplication.java
@@ -0,0 +1,13 @@
+package projeto.dio.projeto.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projeto/dio/projeto/java/ProjetoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.dio.projeto.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
